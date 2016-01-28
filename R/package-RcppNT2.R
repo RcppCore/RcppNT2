bf9f4a1689befdf98cc9faa4074ec8f8ac83be8b
Kevin Ushey <kevinushey@gmail.com>
2016-01-28 15:25:17 -0800

    expand package doc

--- R/package-RcppNT2.R
+++ R/package-RcppNT2.R
@@ -1,6 +1,11 @@
 #' RcppNT2
 #'
-#' \code{RcppNT2} provides bindings to the \code{NT2}
-#' numerical template toolbox.
+#' \code{RcppNT2} provides bindings to the
+#' \href{https://github.com/jfalcou/nt2}{Numerical Template Toolbox}
+#' (\code{NT2}). \code{NT2} is an open source C++ library aimed at
+#' simplifying the development, debugging and optimization
+#' of high-performance computing applications by providing a
+#' \strong{Matlab}-like syntax that eases the transition between
+#' prototype and actual application.
 #'
 "_PACKAGE"
